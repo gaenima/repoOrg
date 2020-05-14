632968efaff535d9bce03489393249eb4aec89ee
unknown <gabriela.n.stocco@gmail.com>
2020-05-14 19:35:39 -0300

    Comenzando el detalle de tareas

---
+++ src/app/components/TaskDetail.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import * as mutations from "../store/mutations";
+
+const TaskDetail = ({
+  id,
+  comments,
+  task,
+  isComplete,
+  groups,
+  setTaskCompletition,
+}) => (
+  <div>
+    <div>
+      <input value={task.name} />
+    </div>
+    <div>
+      <button onClick={() => setTaskCompletition(id, !isComplete)}>
+        {isComplete ? `Reabrir` : `Completar`}
+      </button>
+    </div>
+
+    <div>
+      <select>
+        {groups.map((group) => (
+          <option key={group.id} value={group.id}>
+            {group.name}
+          </option>
+        ))}
+      </select>
+    </div>
+    <div>
+      <Link to="/dashboard">
+        <button>Realizado</button>
+      </Link>
+    </div>
+  </div>
+);
+
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.id;
+  let task = state.tasks.find((task) => task.id === id);
+  let groups = state.groups;
+
+  return {
+    id,
+    task,
+    groups,
+    isComplete: task.isComplete,
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const id = ownProps.match.params.id;
+  return {
+    setTaskCompletition(id, isComplete) {
+      dispatch(mutations.setTaskCompletion(id, isComplete));
+    },
+  };
+};
+
+export const ConnectTaskDetail = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TaskDetail);
---
+++ src/app/store/sagas.mock.js
@@ -0,0 +1,14 @@
+import { take, put, select } from "redux-saga/effects";
+import { v4 as uuidv4 } from "uuid";
+import * as mutations from "./mutations";
+
+export function* taskCreationSaga() {
+  while (true) {
+    const { groupID } = yield take(mutations.REQUEST_TASK_CREATION);
+    const ownerID = `U1`;
+    const taskID = uuidv4();
+    yield put(mutations.createTask(taskID, groupID, ownerID));
+
+    console.log("Got groupID ", groupID);
+  }
+}
